164f8d70eec32c737bbc7a9d0c7a6be5314cd856
Bkmsx <tien@novum.capital>
2018-07-23 17:15:06 +0700

    Initial Commit

---
+++ TestCustomView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCustomView
+//
+//  Created by Lai Trung Tien on 7/23/18.
+//  Copyright © 2018 Lai Trung Tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
